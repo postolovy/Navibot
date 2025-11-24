61029e0baf732c26b848472a169bb258d5cfe38a
postolovy <postolovvy@gmail.com>
2025-11-23 22:01:41 -0800

    Launch view_robot is finished

---
+++ navibot_ws/src/navibot_description/launch/view_robot.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('navibot_description')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'navibot.urdf.xacro')
+
+    robot_description_content = Command(['xacro ', xacro_file])
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description':  ParameterValue(robot_description_content, value_type=str)}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=["-d", os.path.join(get_package_share_directory("navibot_description"), "rviz", "view_robot.rviz")]
+        ),
+    ])
